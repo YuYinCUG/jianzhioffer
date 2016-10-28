24880710819e992cb1ef7e4659d6ed802f385d2b
YuYinCUG <cugyuyin@gmail.com>
2016-10-28 16:58:59 +0800

    add PrintListReversingly.cpp

---
+++ PrintListReversingly.cpp
@@ -0,0 +1,41 @@
+struct ListNode
+{
+  int m_nKey;
+  ListNode* m_pNext;
+}
+
+//从尾到头打印链表
+
+//用栈实现
+void PrintListReversingly_Iteratively(ListNode* pHead)
+{
+  std::stack<ListNode*> nodes;
+
+  ListNode* pNode = pHead;
+
+  while(pNode != NULL)
+  {
+    nodes.push(pNode);
+    pNode = pNode->m_pNext;
+  }
+
+  while(!nodes.empty())
+  {
+    pNode = nodes.top();
+    printf("%d\t", pNode->m_nKey);
+    nodes.pop();
+  }
+}
+
+//用递归实现
+void PrintListReversingly_Recursively(ListNode* pHead)
+{
+  if(pHead != NULL)
+  {
+    if(pHead->m_pNext!=NULL)
+    {
+      PrintListReversingly_Recursively(pHead->m_pNext);
+    }
+    printf("%d\t",pHead->m_nKey);
+  }
+}
--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # 剑指offer题集
 
+- [面试题4：从尾到头打印链表](https://github.com/YuYinCUG/jianzhioffer/blob/master/PrintListReversingly.cpp)
 - [面试题13：在O(1)时间删除链表结点](https://github.com/YuYinCUG/jianzhioffer/blob/master/DeleteNode.cpp)
 - [面试题16：反转链表](https://github.com/YuYinCUG/jianzhioffer/blob/master/ReverseList.cpp)
